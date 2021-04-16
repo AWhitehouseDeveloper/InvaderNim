94418c2cec2836464c23abe86dbe2867868b736d
DakotaBuckner07 <61479565+DakotaBuckner07@users.noreply.github.com>
2021-04-15 18:13:01 -0600

    Game&Cow
    Worked on Game and Cow Scripts

--- Assets/Scripts/Cow.cs
+++ Assets/Scripts/Cow.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class Cow : MonoBehaviour
 {
+    public enum eType
+    {
+        Default, 
+        Selected,
+        Deleted
+    }
+
+    private eType type = eType.Default;
+    public eType Type { get => type; set => type = value; }
     public Color colorChange = new Color(0, 0, 0, 255);
 
     public void Select()
     {
-        colorChange = new Color(100, 100, 100, 255);
+        colorChange = new Color(150, 150, 150, 255);
+        type = eType.Selected;
     }
 
     public void DeSelect()
     {
-        colorChange = new Color(0, 0, 0, 255);
+        colorChange = new Color(50, 50, 50, 255);
+        type = eType.Default;
     }
 
-    void Start()
+    private void Awake()
     {
         
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if(type == eType.Deleted)
+        {
+            Destroy(this);
+        }
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,16 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Game : MonoBehaviour
 {
+    static List<Cow> cows = new List<Cow>();
+
+    public TMP_Text playerNameText;
+    public StringData p1Name;
+    public StringData p2Name;
+
+    private bool isP1Turn;
+
     public void Start()
     {
         
     }
 
-    private void Update()
+    public void OnFinish()
     {
+        foreach(Cow c in cows)
+        {
+            if(c.Type == Cow.eType.Selected)
+            {
+                c.Type = Cow.eType.Deleted;
+            }
+        }
 
+        isP1Turn = !(isP1Turn);
+        if (isP1Turn) playerNameText.text = p1Name;
+        if (!isP1Turn) playerNameText.text = p2Name;
+    }
+
+    public void AddCow(Cow c)
+    {
+        cows.Add(c);
     }
 }
