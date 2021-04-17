7c69be92d7f0331df3d25a7174a53635c6bb2de6
Alexandrea Whitehouse <AWhitehouse@student.neumont.edu>
2021-04-16 23:03:05 -0600

    EVERYTHING again

--- Assets/Scripts/Cow.cs
+++ Assets/Scripts/Cow.cs
@@ -4,25 +4,57 @@ using UnityEngine;
 
 public class Cow : MonoBehaviour
 {
+    public enum eType
+    {
+        Default, 
+        Selected,
+        Deleted
+    }
+
+    private eType type = eType.Default;
+    public eType Type { get => type; set => type = value; }
     public Color colorChange = new Color(0, 0, 0, 255);
+
+    public GameObject button;
+
+    public void OnClick()
+    {
+        if(type == eType.Selected)
+        {
+            Debug.Log("Selected");
+            DeSelect();
+        }
+        else if (type == eType.Default)
+        {
+            Debug.Log("Unselected");
+            Select();
+        }
+
+        Debug.Log(type);
+    }
 
     public void Select()
     {
-        colorChange = new Color(100, 100, 100, 255);
+        colorChange = new Color(150, 150, 150, 255);
+        type = eType.Selected;
     }
 
     public void DeSelect()
     {
-        colorChange = new Color(0, 0, 0, 255);
+        colorChange = new Color(50, 50, 50, 255);
+        type = eType.Default;
     }
 
-    void Start()
+    private void Awake()
     {
-        
+        Game.Instance().AddCow(this);
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if(type == eType.Deleted)
+        {
+            button.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,16 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Game : MonoBehaviour
 {
-    public void Start()
+    public static List<Cow> cows = new List<Cow>();
+
+    public GameObject userPanel;
+    public GameObject winnerPanel;
+    public GameObject firstTurnPanel;
+    public GameObject forfeitPanel;
+
+    public TMP_Text playerNameText;
+    public TMP_Text playerFirstText;
+    public TMP_Text playerWinnerText;
+    public StringData p1Name;
+    public StringData p2Name;
+    public StringData winnerName;
+
+    private bool isP1Turn = true;
+    int visiableCows;
+
+    private static Game instance = new Game();
+    public static Game Instance() 
     {
-        
+        return instance; 
     }
 
-    private void Update()
+    private void Start()
     {
+        OnFirstSelect();
+    }
 
+    public void OnToGame()
+    {
+        firstTurnPanel.SetActive(false);
+        userPanel.SetActive(true);
+        visiableCows = cows.Count;
+
+        if (isP1Turn) playerNameText.text = p1Name;
+        if (!isP1Turn) playerNameText.text = p2Name;
+    }
+
+    public void OnFinish()
+    {
+        foreach(Cow c in cows)
+        {
+            if(c.Type == Cow.eType.Selected)
+            {
+                c.Type = Cow.eType.Deleted;
+                visiableCows--;
+            }
+        }
+        Debug.Log(visiableCows);
+        if(visiableCows == 0)
+        {
+            if (isP1Turn) winnerName = p2Name;
+            else winnerName = p1Name;
+            winnerPanel.SetActive(true);
+            userPanel.SetActive(false);
+            playerWinnerText.text = winnerName + " won";
+        }
+
+        isP1Turn = !(isP1Turn);
+        if (isP1Turn) playerNameText.text = p1Name.value;
+        if (!isP1Turn) playerNameText.text = p2Name.value;
+
+        Debug.Log(isP1Turn);
+    }
+
+    public void OnForfeit()
+    {
+        forfeitPanel.SetActive(true);
+        userPanel.SetActive(false);
+    }
+
+    public void OnForfeitNot()
+    {
+        forfeitPanel.SetActive(false);
+        OnToGame();
+    }
+
+    public void OnForfeitYes()
+    {
+        if (isP1Turn) winnerName = p2Name;
+        else winnerName = p1Name;
+        forfeitPanel.SetActive(false);
+        winnerPanel.SetActive(true);
+        userPanel.SetActive(false);
+        playerWinnerText.text = winnerName;
+    }
+
+    public void OnFirstSelect()
+    {
+        firstTurnPanel.SetActive(true);
+        userPanel.SetActive(false);
+        isP1Turn = (Random.Range(0, 2) == 1 ? true : false);
+        if(isP1Turn)
+        {
+           playerFirstText.text = p1Name +" \nis going first";
+        }
+        else
+        {
+           playerFirstText.text = p2Name +" \nis going first";
+        }
+    }
+
+    public void AddCow(Cow c)
+    {
+        cows.Add(c);
     }
 }
